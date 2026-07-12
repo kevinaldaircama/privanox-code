b4a8d63620c6f276dbc7d7c21225b09f6ea7640b
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-07-12 13:39:39 -0500

    Update updater.go

--- internal/sys/updater.go
+++ internal/sys/updater.go
@@ -15,7 +15,7 @@ const (
 	// CurrentVersion indica la versión actual en ejecución
 	CurrentVersion = "7.9"
 	// RemoteVersionURL es el archivo en GitHub que dice la última versión disponible
-	RemoteVersionURL = "https://raw.githubusercontent.com/Depwisescript/BOT-TELEGRAM-VPN/main/version.txt"
+	RemoteVersionURL = "https://raw.githubusercontent.com/kevinaldaircama/privanox-code/main/version.txt"
 )
 
 // CheckForUpdate verifica si hay una actualización disponible comparando la versión local con la remota.
